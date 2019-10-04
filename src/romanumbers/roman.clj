e52d78ff208fee6c3809a3d2b5b2096794f21b2e
Diego <diego.sanches@gmail.com>
2019-10-04 17:39:29 +0000

    refactor succ

--- src/romanumbers/roman.clj
+++ src/romanumbers/roman.clj
@@ -1,9 +1,25 @@
 (ns romanumbers.roman)
 
+(defn one
+  []
+  "I")
+
+(defn five
+  []
+  "V")
+
+(defn succ
+  [x]
+  (str x (one)))
+
+(defn decc
+  [x]
+  (str (one) x))
+
 (defn convert
   [x]
-  (cond (= x 1) "I"
-        (= x 2) "II"
-        (= x 3) "III"
-        (= x 4) "IV"
-        (= x 5) "V"))
+  (cond (= x 1) (one)
+        (= x 2) (succ (one))
+        (= x 3) (succ (succ (one)))
+        (= x 4) (decc (five))
+        (= x 5) (five)))
