e87f79f19b9c1505ff167b59dff0b87d1c63564a
Diego Sanchez <diego.sanchez@etermax.com>
2019-10-08 14:51:35 -0300

    remove cond

--- src/romanumbers/roman.clj
+++ src/romanumbers/roman.clj
@@ -3,7 +3,7 @@
 (defn one
   []
   "I")
-(conj)
+
 (defn succ
   [x]
   (def nextOf {
@@ -16,13 +16,5 @@
 
 (defn convert
   [x]
-  (cond (= x 1) (one)
-        (= x 2) (succ(one))
-        (= x 3) (succ(succ(one)))
-        (= x 4) (succ(succ(succ(one))))
-        (= x 5) (succ(succ(succ(succ(one)))))
-        (= x 6) (succ(succ(succ(succ(succ(one))))))
-        (= x 7) (succ(succ(succ(succ(succ(succ(one)))))))
-        (= x 8) (succ(succ(succ(succ(succ(succ(succ(one))))))))
-        (= x 9) (succ(succ(succ(succ(succ(succ(succ(succ(one)))))))))
-        ))
+  (def succ-fns (apply comp (repeat (- x 1) succ)))
+  (succ-fns (one)))
